162ae898e38e6d6be5a3840e87fb82f70b514fe4
unknown <abilsabili50@gmail.com>
2022-06-22 14:00:51 +0700

    Menambahkan plugin-plugin

--- src/config.php
+++ src/config.php
@@ -3,7 +3,7 @@
 	$dbhost = "localhost";
 	$dbuser = "root";
 	$dbpass = "";
-	$dbname = "jadwal_mapel";
+	$dbname = "mapel_baru";
 	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 	function sqlquery($query){
